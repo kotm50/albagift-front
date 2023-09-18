5dcd277baa36e16d8ae0dafcd764962e26e2b443
kotm50 <koreatm50@gmail.com>
2023-09-18 14:25:23 +0900

    본인인증

--- src/Component/User/Certification.js
+++ src/Component/User/Certification.js
@@ -47,7 +47,10 @@ function Certification() {
           className="mx-auto p-2 grid grid-cols-1 gap-3 w-full"
         >
           <div className="w-full">
-            <button className="transition duration-100 w-full p-2 bg-stone-700 hover:bg-stone-950 text-white rounded hover:animate-wiggle">
+            <button
+              className="transition duration-100 w-full p-2 bg-stone-700 hover:bg-stone-950 text-white rounded hover:animate-wiggle"
+              disabled={integrityValue === ""}
+            >
               본인인증
             </button>
           </div>

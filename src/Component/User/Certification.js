524c846a6ca6dc775ad5ba9376706cab9f5af125
kotm50 <koreatm50@gmail.com>
2023-09-18 14:05:37 +0900

    본인인증 버튼추가

--- src/Component/User/Certification.js
+++ src/Component/User/Certification.js
@@ -18,28 +18,28 @@ function Certification() {
     axios
       .post("/api/v1/common/nice/sec/req")
       .then(res => {
-        console.log(res);
+        setTokenId(res.data.tokenVersionId);
+        setEncData(res.data.encData);
+        setIntegrityValue(res.data.integrityValue);
       })
       .catch(e => {
         console.log(e);
       });
-    setTokenId("");
-    setEncData("");
-    setIntegrityValue("");
-    console.log(tokenId, encData, integrityValue);
   };
   return (
     <div className="container mx-auto h-full pt-10">
       <form action="https://nice.checkplus.co.kr/CheckPlusSafeModel/service.cb">
+        <input type="hidden" value={tokenId} />
+        <input type="hidden" value={encData} />
+        <input type="hidden" value={integrityValue} />
         <div
           id="loginArea"
           className="mx-auto p-2 grid grid-cols-1 gap-3 w-full"
         >
           <div className="w-full">
-            <button
-              className="transition duration-100 w-full p-2 text-black rounded hover:animate-wiggle"
-              onClick={e => console.log(e)}
-            ></button>
+            <button className="transition duration-100 w-full p-2 bg-stone-700 hover:bg-stone-950 text-white rounded hover:animate-wiggle">
+              본인인증
+            </button>
           </div>
         </div>
       </form>

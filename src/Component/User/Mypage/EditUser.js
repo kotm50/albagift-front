3d50110f2e304c0a9c31fe559bc18c83bca279b0
kotm50 <koreatm50@gmail.com>
2023-09-25 13:18:27 +0900

    설명창 조금 하얗게

--- src/Component/User/Mypage/EditUser.js
+++ src/Component/User/Mypage/EditUser.js
@@ -351,7 +351,7 @@ function EditUser(props) {
             수정하기
           </button>
         </div>
-        <div className="text-xs text-center xl:text-right p-1 bg-gray-100">
+        <div className="text-xs text-center xl:text-right p-1">
           이름/연락처를 수정하려면{" "}
           <span className="font-neoextra">본인인증</span>이 필요합니다
         </div>

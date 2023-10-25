fcdc0673336f83dce1d677c081c30c0de6193920
kotm50 <koreatm50@gmail.com>
2023-10-25 13:15:12 +0900

    화면

--- src/Component/KakaoTest.js
+++ src/Component/KakaoTest.js
@@ -1,37 +1,16 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { dayModal } from "../Reducer/modalSlice";
-
 function KakaoTest() {
-  const dispatch = useDispatch();
-  const modalState = useSelector(state => state.modal);
-  useEffect(() => {
-    console.log(modalState);
-  }, [modalState]);
-  const day = () => {
-    dispatch(
-      dayModal({
-        oneDay: "Y",
-        lastOpen: new Date(),
-      })
-    );
-  };
-  const hour = () => {
-    dispatch(
-      dayModal({
-        oneDay: "N",
-        lastOpen: new Date(),
-      })
-    );
-  };
   return (
-    <div className="container mx-auto flex justify-start gap-x-3">
-      <button className="p-2 bg-green-500 text-white" onClick={day}>
-        하루닫기
-      </button>
-      <button className="p-2 bg-gray-500 text-white" onClick={hour}>
-        하루안닫기
-      </button>
+    <div>
+      <h1 className="text-center text-xl xl:text-4xl font-medium mt-4">
+        구매가 완료되었습니다.
+        <br className="block xl:hidden" /> 이용해주셔서 감사합니다.
+      </h1>
+      <div className="countainer mx-auto bg-indigo-50 p-4 mt-5 rounded-lg">
+        <div className="xl:text-lg text-center">
+          <strong className="text-rose-500 text-xl xl:text-2xl">3</strong> 초 후
+          쿠폰리스트로 이동합니다
+        </div>
+      </div>
     </div>
   );
 }

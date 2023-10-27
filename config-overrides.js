b3118e62744587f71b47c42b089e5c77ce3f4998
kotm50 <koreatm50@gmail.com>
2023-10-27 15:48:43 +0900

    캐시버스팅팅

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,7 @@
 module.exports = {
   webpack: function (config, env) {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, "0");
-    const minutes = String(now.getMinutes()).padStart(2, "0");
-    const seconds = String(now.getSeconds()).padStart(2, "0");
-
-    const result = `${hours}${minutes}${seconds}`;
-    config.output.filename = `static/js/[name].[hash:8].js?ver=${result}`;
-    config.output.chunkFilename = `static/js/[name].[hash:8].chunk.js?ver=${result}`;
+    config.output.filename = "static/js/[name].[hash:8].js";
+    config.output.chunkFilename = "static/js/[name].[hash:8].chunk.js";
     return config;
   },
 };

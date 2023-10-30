b53a62c37936116d27d9129b8f57debe11017746
kotm50 <koreatm50@gmail.com>
2023-10-30 11:22:38 +0900

    모바일테스트

--- src/Component/User/Certification.js
+++ src/Component/User/Certification.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import axios from "axios";
 
@@ -8,14 +8,13 @@ import Loading from "../Layout/Loading";
 
 function Certification() {
   const location = useLocation();
-
+  const navi = useNavigate();
   const parsed = queryString.parse(location.search);
   const token_version_id = parsed.token_version_id || "";
   const enc_data = parsed.enc_data || "";
   const integrity_value = parsed.integrity_value || "";
 
   useEffect(() => {
-    console.log(parsed);
     if (integrity_value === "" && enc_data === "" && token_version_id === "") {
       getData();
     } else {
@@ -60,9 +59,16 @@ function Certification() {
       encData: enc_data,
       integrityValue: integrity_value,
     };
-
-    window.opener.parentCallback(data);
-    window.close();
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    console.log(isMobile ? "모바일" : "모바일아님");
+    if (!isMobile) {
+      window.opener.parentCallback(data);
+      window.close();
+    } else {
+      navi(
+        `/cert?=tokenVersionId=${data.tokenVersionId}&encData=${data.encData}&intergrityValue=${data.integrityValue}`
+      );
+    }
   };
   return (
     <div>

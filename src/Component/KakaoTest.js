c788c76ea3633db7d0b4c39da6bf8e0a1fd8418d
kotm50 <koreatm50@gmail.com>
2023-11-08 15:49:45 +0900

    사이트맵수정

--- src/Component/KakaoTest.js
+++ src/Component/KakaoTest.js
@@ -1,47 +1,30 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import React from "react";
 
-import axios from "axios";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  LineShareButton,
+  LineIcon,
+} from "react-share";
 
-function KakaoTest() {
-  const user = useSelector(state => state.user);
-  const [trId, setTrId] = useState("");
-
-  const chkCoupon = async c => {
-    const data = {
-      trId: trId,
-    };
-    await axios
-      .post("/api/v1/shop/goods/coupons", data, {
-        headers: { Authorization: user.accessToken },
-      })
-      .then(res => {
-        console.log(res);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  };
-
+export default function KakaoTest() {
+  const currentUrl = "https://albagift.com/detail/G00001331259";
+  console.log(currentUrl);
   return (
-    <div className="container mx-auto">
-      <input
-        type="text"
-        className="m-2 p-2 border"
-        id="page"
-        name="page"
-        value={trId}
-        onChange={e => setTrId(e.currentTarget.value)}
-      />
-      <button
-        type="submit"
-        className="bg-green-500 p-2 text-white"
-        onClick={e => chkCoupon()}
-      >
-        후아
-      </button>
-    </div>
+    <>
+      <div className="container mx-auto">
+        <FacebookShareButton style={{ marginRight: "20px" }} url={currentUrl}>
+          <FacebookIcon size={48} round={true} borderRadius={24}></FacebookIcon>
+        </FacebookShareButton>
+        <TwitterShareButton style={{ marginRight: "20px" }} url={currentUrl}>
+          <TwitterIcon size={48} round={true} borderRadius={24}></TwitterIcon>
+        </TwitterShareButton>
+        <LineShareButton url={currentUrl}>
+          <LineIcon size={48} round={true} borderRadius={24}></LineIcon>
+        </LineShareButton>
+      </div>
+    </>
   );
 }
-
-export default KakaoTest;

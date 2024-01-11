d1a17878a02a33414364da4190dbdd75493a4e71
kotm50 <koreatm50@gmail.com>
2024-01-11 09:41:21 +0900

    메뉴수정

--- src/Component/Layout/Menu/GiftBrand.js
+++ src/Component/Layout/Menu/GiftBrand.js
@@ -1,31 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { useLocation, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import axios from "axios";
 
 function GiftBrand(props) {
   const user = useSelector(state => state.user);
-  const location = useLocation();
   const [load, setLoad] = useState(false);
   const [brandList, setBrandList] = useState([]);
 
   useEffect(() => {
-    const parts = location.pathname.split("/");
-    getBrandList(parts[2]);
-    //eslint-disable-next-line
-  }, [location]);
-
-  useEffect(() => {
-    setLoad(false);
-    if (brandList.length > 0) {
-      setLoad(true);
+    if (props.category === "") {
+      setBrandList([]);
     } else {
-      setLoad(false);
+      getBrandList(props.category);
     }
-  }, [brandList]);
+    //eslint-disable-next-line
+  }, [props.category]);
 
   const getBrandList = async n => {
+    setLoad(false);
     setBrandList([]);
     let listUrl = "/api/v1/shop/brand/list";
     if (n !== "") {
@@ -35,47 +29,54 @@ function GiftBrand(props) {
       .get(listUrl, { token: user.accessToken })
       .then(res => {
         setBrandList(res.data.brandsList);
+        setLoad(true);
       })
       .catch(e => {
         console.log(e);
       });
   };
   return (
-    <div>
+    <>
       {load ? (
-        <div className="container mx-auto text-lg flex flex-row flex-nowrap lg:flex-wrap gap-3 p-2 lg:max-h-72 overflow-auto giftCategoryMenu bg-indigo-100">
-          {brandList.map((brand, idx) => (
-            <Link
-              to={`/list/${props.cateNum}/${brand.brandCode}`}
-              className={
-                String(brand.brandSeq) === props.brandNum
-                  ? "bg-blue-500 py-2 px-4 text-white giftcategory text-sm rounded-lg border lg:w-1/12"
-                  : "text-black bg-white py-2 px-4 hover:bg-blue-500 hover:text-white hover:drop-shadow-lg giftcategory text-sm rounded-lg border lg:w-1/12"
-              }
-              key={idx}
-            >
-              <div className="w-16 h-16 mx-auto p-1 rounded bg-white overflow-hidden">
-                <img
-                  src={brand.brandIConImg}
-                  alt={`${brand.brandName}의 로고`}
-                  className="w-16 h-auto mx-auto"
-                />
-              </div>
-              <div className="w-full overflow-hidden text-ellipsis text-center mt-1">
-                {brand.brandName}
-              </div>
-            </Link>
-          ))}
-        </div>
+        <>
+          {brandList.length > 0 ? (
+            <div className="grid grid-cols-8 gap-x-2 gap-y-2 w-full">
+              {brandList.map((brand, idx) => (
+                <Link
+                  to={`/list/${props.category}/${brand.brandCode}`}
+                  className={
+                    String(brand.brandSeq) === props.brandNum
+                      ? "bg-blue-500 py-2 px-4 text-white giftcategory text-sm rounded-lg border"
+                      : "text-black bg-white py-2 px-4 hover:bg-blue-500 hover:text-white hover:drop-shadow-lg giftcategory text-sm rounded-lg border"
+                  }
+                  key={idx}
+                >
+                  <div className="w-24 h-24 mx-auto p-1 rounded bg-white overflow-hidden">
+                    <img
+                      src={brand.brandIConImg}
+                      alt={`${brand.brandName}의 로고`}
+                      className="w-24 h-auto mx-auto"
+                    />
+                  </div>
+                  <div className="w-full overflow-hidden text-ellipsis text-center mt-1">
+                    {brand.brandName}
+                  </div>
+                </Link>
+              ))}
+            </div>
+          ) : (
+            <div className="text-center">브랜드가 없습니다.</div>
+          )}
+        </>
       ) : (
-        <div className="container mx-auto text-lg flex flex-row flex-nowrap lg:flex-wrap gap-3 p-2 lg:max-h-72 overflow-auto giftCategoryMenu bg-indigo-100">
-          <div className="aniamte-pulse  bg-white py-2 px-4 giftcategory text-sm rounded-lg border lg:w-1/12">
-            <div className="w-16 h-16 mx-auto p-1 rounded bg-slate-200 overflow-hidden"></div>
+        <div className="grid grid-cols-8">
+          <div className="aniamte-pulse  bg-white py-2 px-4 text-sm rounded-lg border">
+            <div className="w-24 h-24 mx-auto p-1 rounded bg-slate-200 overflow-hidden"></div>
             <div className="w-full bg-slate-200 h-2 mt-1"></div>
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
 
---
+++ src/Component/Layout/Menu_back/GiftCategory.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { category } from "../../Data/Category";
+import { AiFillHome } from "react-icons/ai";
+import CategoryIcons from "./CategoryIcons";
+
+function GiftCategory(props) {
+  return (
+    <>
+      <div className="flex flex-row flex-nowrap">
+        <Link
+          to="/"
+          className="bg-teal-500 text-white  p-2 hover:bg-teal-100 hover:text-black giftcategory"
+        >
+          <div>
+            <AiFillHome size={24} />
+          </div>
+        </Link>
+        <div
+          id="touch-target"
+          className="container mx-auto flex flex-row flex-nowrap overflow-x-auto giftCategoryMenu"
+        >
+          <Link
+            to="/list"
+            className={
+              props.path === "/list"
+                ? props.cateno
+                  ? "bg-white text-gray-500 p-2 hover:bg-teal-100 hover:text-black giftcategory"
+                  : "bg-teal-50 text-gray-500 border-b-2 border-teal-500 p-2 hover:bg-teal-100 hover:text-black giftcategory"
+                : "bg-white text-gray-500 p-2 hover:bg-teal-100 hover:text-black giftcategory"
+            }
+          >
+            <div>전체상품</div>
+          </Link>
+          {category.map(cat => (
+            <Link
+              to={`/list/${cat.category1Seq}`}
+              className={
+                Number(props.cateno) === cat.category1Seq
+                  ? "bg-teal-50 text-gray-500 p-2 border-b-2 border-teal-500 hover:bg-teal-100 hover:text-black giftcategory"
+                  : "bg-white text-gray-500 p-2 hover:bg-teal-100 hover:text-black giftcategory"
+              }
+              key={cat.category1Seq}
+            >
+              <div className="flex flex-row flex-nowrap gap-1">
+                <CategoryIcons num={cat.category1Seq} />
+                {cat.category1Name}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default GiftCategory;

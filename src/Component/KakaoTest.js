14243916d217ee5cb8a4ec1af86d74276fcb66d4
kotm50 <koreatm50@gmail.com>
2024-02-21 17:19:56 +0900

    파일

--- src/Component/KakaoTest.js
+++ src/Component/KakaoTest.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import test from "https://api-echo-bucket.s3.ap-northeast-2.amazonaws.com/images/367b34e3-0aaa-4cc7-8b15-cb9e2305861e.jpg";
 
 function KakaoTest() {
   const navi = useNavigate();
@@ -31,7 +30,10 @@ function KakaoTest() {
       >
         프로모션가입
       </button>
-      <img src={test} alt="ㅇㅇ" />
+      <img
+        src="https://api-echo-bucket.s3.ap-northeast-2.amazonaws.com/images/367b34e3-0aaa-4cc7-8b15-cb9e2305861e.jpg"
+        alt="ㅇㅇㅇ"
+      />
     </div>
   );
 }

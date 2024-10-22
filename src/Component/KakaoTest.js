3a24c10a2f4fbbc4e0cebc4351e8f68a1315e0df
kotm50 <koreatm50@gmail.com>
2024-10-22 09:54:38 +0900

    코티잡스 첫커밋

--- src/Component/KakaoTest.js
+++ src/Component/KakaoTest.js
@@ -1,90 +1,39 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import axios from "axios";
 //import axiosInstance from "../Api/axiosInstance";
-import { useSelector } from "react-redux";
-
 function KakaoTest() {
-  const user = useSelector(state => state.user);
-  const fileInputRef = useRef(null);
-
-  const [selectedFile, setSelectedFile] = useState("");
-
-  const handleFileSelect = event => {
-    // 파일이 선택되지 않은 경우 (사용자가 취소 버튼을 클릭한 경우)
-    if (event.target.files.length === 0) {
-      setSelectedFile(""); // selectedFile 상태를 빈 문자열로 설정
-      return; // 함수 종료
-    }
-
-    const file = event.target.files[0];
-    setSelectedFile(file);
-  };
-
-  const saveIt = async () => {
-    /*
+  const sendIt = async () => {
     const data = {
-      file: selectedFile,
+      msg: "테스트입니다",
+      smsType: "S",
+      sPhone1: "010",
+      sPhone2: "7745",
+      sPhone3: "6059",
+      rPhone: "010-8031-5450",
     };
-    */
-
-    const formData = new FormData();
-    formData.append("file", selectedFile);
+    console.log(data);
     try {
-      const response = await axios.post(
-        "/api/v1/shop/admin/add/intv/excel",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            Authorization: user.accessToken,
-          },
-        }
-      );
-      console.log(response);
-    } catch (error) {
-      console.log(error);
+      const res = await axios.post("/api/v1/formMail/sendSms", data);
+      console.log(res);
+    } catch (e) {
+      // 오류 메시지와 상태 코드를 함께 출력
+      if (e.response) {
+        console.error(`Error: ${e.message}, Status: ${e.response.status}`);
+      } else {
+        console.error(`Error: ${e.message}`);
+      }
     }
   };
-
-  const saveIt2 = async () => {
-    const data = {
-      file: selectedFile,
-    };
-
-    try {
-      const response = await axios.post("/api/v1/shop/add/intv/excel", data, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          Authorization: user.accessToken,
-        },
-      });
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <>
-      <div className="p-2 bg-[#eaeaea] flex justify-start gap-x-2 container mx-auto">
-        <div className="w-full lg:w-[20%] px-4 lg:py-6 py-2 font-neoextra truncate break-keep text-xs lg:text-lg lg:text-right bg-gray-100">
-          테스트
-        </div>
-        <div className="w-full lg:w-fit lg:flex-1 text-xs lg:text-lg font-neo lg:px-4 lg:py-6">
-          <input
-            ref={fileInputRef}
-            type="file"
-            accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-            multiple
-            onChange={handleFileSelect}
-          />
-          테스트
-        </div>
-        <button className="bg-[#333] p-2 text-white" onClick={() => saveIt()}>
-          버튼1
-        </button>
-        <button className="bg-[#dcf] p-2 text-white" onClick={() => saveIt2()}>
-          버튼2
+      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-fit  p-2 bg-white border rounded-lg">
+        <button
+          className="p-2 bg-green-600 text-white"
+          onClick={() => {
+            sendIt();
+          }}
+        >
+          문자전송 테스트
         </button>
       </div>
     </>

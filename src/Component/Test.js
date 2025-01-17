fc12292e70fd6685db6156945b67cbfa9fe50b2f
kotm50 <koreatm50@gmail.com>
2025-01-17 09:58:27 +0900

    모달크기조정

--- src/Component/Test.js
+++ src/Component/Test.js
@@ -1,37 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./doc/Modal";
 
 function Test() {
+  const [modalOn, setModalOn] = useState(false);
+  const [modalCount, setModalCount] = useState(0);
   return (
-    <div className="w-[1000px] mx-auto">
-      <h2 className="text-6xl text-center font-neoextra text-green-800 my-8">
-        코리아티엠 방문예약 주문서
-      </h2>
-      <p className="text-lg text-center font-neo my-8 leading-7">
-        코리아티엠을 방문해 주셔서 감사합니다. 방문하신 모든 분들께
-        <br />
-        음료와 간식을 무료로 제공해 드리고 있사오니 원하시는 메뉴를 선택해
-        주세요
-      </p>
-      <div className="flex justify-between w-[95%] mx-auto">
-        <div className="flex gap-x-2 p-2 border-b border-green-800 w-[22%]">
-          <p className="text-lg text-left w-[40%]">지점명</p>
-          <input type="text" className="border-0 w-full text-left" />
-        </div>
-        <div className="flex gap-x-2 p-2 border-b border-green-800 w-[30%]">
-          <p className="text-lg text-left w-[40%]">연락처</p>
-          <input type="text" className="border-0 w-full text-left" />
-        </div>
-        <div className="flex gap-x-2 p-2 border-b border-green-800 w-[18%]">
-          <p className="text-lg text-left w-[40%]">인원</p>
-          <input type="text" className="border-0 w-full text-left" />
-        </div>
-        <div className="flex gap-x-2 p-2 border-b border-green-800 w-[23%]">
-          <p className="text-lg text-left w-[40%]">일시</p>
-          <input type="text" className="border-0 w-full text-left" />
-        </div>
+    <>
+      <div className="w-[1000px] mx-auto">
+        <button
+          className="text-blue-500 hover:text-violet-700 p-2 text-xs w-full"
+          onClick={e => {
+            e.preventDefault();
+            setModalCount(2);
+            setModalOn(true);
+          }}
+        >
+          상세보기
+        </button>
       </div>
-      <div className="w-full h-[2px] bg-green-800 my-8" />
-    </div>
+
+      {modalOn ? (
+        <Modal
+          modalCount={modalCount}
+          setModalOn={setModalOn}
+          setModalCount={setModalCount}
+        />
+      ) : null}
+    </>
   );
 }
 
--- src/Component/doc/Modal.js
+++ src/Component/doc/Modal.js
@@ -14,70 +14,66 @@ function Modal(props) {
   let domainExtension = parts[parts.length - 1]; // 'com'
   return (
     <>
-      <div
-        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none max-h-[95vh]"
-        onClick={e => {
-          props.setModalOn(false);
-          props.setModalCount(0);
-        }}
-      >
-        <div className="relative lg:w-auto my-6 mx-auto w-11/12 lg:max-w-3xl">
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none max-h-108 p-6">
-            <h3 className="text-lg font-neoextra mb-3">
-              {props.modalCount === 1
-                ? "개인정보 처리방침"
-                : props.modalCount === 2
-                ? "이용약관"
-                : props.modalCount === 3
-                ? "E-Mail 무단수집 거부"
-                : props.modalCount === 4
-                ? "개인정보 수집 이용 동의"
-                : props.modalCount === 5
-                ? "마케팅 정보 수신 동의"
-                : props.modalCount === 6
-                ? "추천 채용안내를 위한 개인정보 처리"
-                : null}
-            </h3>
-            <div className="relative p-2 lg:p-6 flex-auto overflow-y-auto">
-              {props.modalCount === 1 ? (
-                <Privacy
-                  domainName={domainName}
-                  domainExtension={domainExtension}
-                />
-              ) : props.modalCount === 2 ? (
-                <Terms
-                  domainName={domainName}
-                  domainExtension={domainExtension}
-                />
-              ) : props.modalCount === 3 ? (
-                <RefusalEmail
-                  domainName={domainName}
-                  domainExtension={domainExtension}
-                />
-              ) : props.modalCount === 4 ? (
-                <Privacy2
-                  domainName={domainName}
-                  domainExtension={domainExtension}
-                />
-              ) : props.modalCount === 5 ? (
-                <Marketing
-                  domainName={domainName}
-                  domainExtension={domainExtension}
-                />
-              ) : props.modalCount === 6 ? (
-                <JobRec
-                  domainName={domainName}
-                  domainExtension={domainExtension}
-                />
-              ) : null}
-            </div>
-          </div>
-          <div className="text-white drop-shadow-lg">
-            창을 닫으려면 아무 곳이나 눌러주세요
-          </div>
+      <div className="fixed top-1/2 left-1/2 min-w-[200px] min-h-[100px] h-[95vh] overflow-hidden -translate-x-1/2 -translate-y-1/2 z-[2000] bg-white rounded-lg p-10">
+        <h3 className="text-lg font-neoextra mb-3">
+          {props.modalCount === 1
+            ? "개인정보 처리방침"
+            : props.modalCount === 2
+            ? "이용약관"
+            : props.modalCount === 3
+            ? "E-Mail 무단수집 거부"
+            : props.modalCount === 4
+            ? "개인정보 수집 이용 동의"
+            : props.modalCount === 5
+            ? "마케팅 정보 수신 동의"
+            : props.modalCount === 6
+            ? "추천 채용안내를 위한 개인정보 처리"
+            : null}
+        </h3>
+        <button
+          className="fixed w-[36px] h-[36px] font-neoextra top-10 right-10"
+          onClick={() => {
+            props.setModalCount(0);
+            props.setModalOn(false);
+          }}
+        >
+          x
+        </button>
+        <div className="relative p-2 lg:p-6 flex-auto h-[90vh] overflow-y-auto">
+          {props.modalCount === 1 ? (
+            <Privacy
+              domainName={domainName}
+              domainExtension={domainExtension}
+            />
+          ) : props.modalCount === 2 ? (
+            <Terms domainName={domainName} domainExtension={domainExtension} />
+          ) : props.modalCount === 3 ? (
+            <RefusalEmail
+              domainName={domainName}
+              domainExtension={domainExtension}
+            />
+          ) : props.modalCount === 4 ? (
+            <Privacy2
+              domainName={domainName}
+              domainExtension={domainExtension}
+            />
+          ) : props.modalCount === 5 ? (
+            <Marketing
+              domainName={domainName}
+              domainExtension={domainExtension}
+            />
+          ) : props.modalCount === 6 ? (
+            <JobRec domainName={domainName} domainExtension={domainExtension} />
+          ) : null}
         </div>
       </div>
-      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+      <div
+        className="fixed top-0 left-0 w-[100vw] h-[100vh] overflow-y-hidden bg-black bg-opacity-50 z-[1000]"
+        onClick={() => {
+          props.setModalCount(0);
+          props.setModalOn(false);
+        }}
+      ></div>
     </>
   );
 }
